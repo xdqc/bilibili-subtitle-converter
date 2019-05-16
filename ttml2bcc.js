c8e07d6c83c29b1f7f7f9e4df439933d9ef1e781
xdqc <qichen.d@outlook.com>
2019-05-16 23:19:16 +1200

    Generate duo-lang subtitle

--- ttml2bcc.js
+++ ttml2bcc.js
@@ -8,18 +8,25 @@ if (!file) {
     return 1;
 }
 
+const zhCN = file.endsWith('zh-Hans.ttml');
+
 
 fs.readFile('./' + file, (err, data) => {
-
+    
     if (err) {
         console.error(err);
         return 1;
     }
 
-    fs.writeFile(file + '.bcc', JSON.stringify(buildBcc(data), null, 2), (err) => console.log(err ? err : file + ' Succeed!'));
+    if (zhCN) {
+        var enFile = file.replace('zh-Hans', 'en');
+        var eData = fs.readFileSync('./' + enFile);
+    }
+
+    fs.writeFile(file + '.bcc', JSON.stringify(buildBcc(data, eData), null, 2), (err) => console.log(err ? err : file + ' Succeed!'));
 });
 
-function buildBcc(ttml) {
+function buildBcc(ttml, eTtml) {
     const bcc = {
         "font_size": 0.4,
         "font_color": "#FFFFFF",
@@ -30,31 +37,41 @@ function buildBcc(ttml) {
     }
 
     const lines = ttml.toString().split('\n');
+    const eLines = eTtml ? eTtml.toString().split('\n') : undefined;
 
     const firstLine = lines.findIndex(line => line.startsWith("<p "));
-    const lastLine = lines.findIndex(line => line.startsWith("</div>"));
+    const lastLine = lines.findIndex(line => line.startsWith("</body>"));
 
     for (let i = firstLine; i < lastLine; i++) {
         const doc = new dom().parseFromString(lines[i]);
 
-        const time1 = hhmmss2second(xpath.select1('/p/@begin', doc).value);
-        let time2 = hhmmss2second(xpath.select1('/p/@end', doc).value);
-        const text = xpath.select('string(//p)', doc);
+        // const time1 = hhmmss2second(xpath.select1('/p/@begin', doc).value);
+        // let time2 = hhmmss2second(xpath.select1('/p/@end', doc).value);
 
-        // prevent subtitle overlapping or showing prev and current line at the same time
-        if (i < lastLine - 1) {
-            const nextDoc = new dom().parseFromString(lines[i + 1]);
-            let nextTime1 = hhmmss2second(xpath.select1('/p/@begin', nextDoc).value);
-            if (parseFloat(nextTime1) < parseFloat(time2)) {
-                time2 = nextTime1;
-            }
+        // // prevent subtitle overlapping or showing prev and current line at the same time
+        // if (i < lastLine - 1) {
+        //     const nextDoc = new dom().parseFromString(lines[i + 1]);
+        //     let nextTime1 = hhmmss2second(xpath.select1('/p/@begin', nextDoc).value);
+        //     if (parseFloat(nextTime1) < parseFloat(time2)) {
+        //         time2 = nextTime1;
+        //     }
+        // }
+        
+       const time1 = (xpath.select1('/p/@t', doc).value) / 1000;
+       const time2 = time1 + (xpath.select1('/p/@d', doc).value) / 1000;
+        let text = xpath.select('string(//p)', doc);
+
+        if (zhCN) {
+            const eDoc = new dom().parseFromString(eLines[i]);
+            const eText = xpath.select('string(//p)', eDoc);
+            text = eText + '\n' + text;
         }
-
+       
         bcc.body.push({
             'from': time1,
             'to': time2,
             'location': 2,
-            'content': text
+            'content': text,
         });
     }
 
